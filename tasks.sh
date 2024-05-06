1c266dc3bcf07a58f7111c86ec2ad570e106ce2b
Arno Cellarier <git@arno.cl>
2024-05-06 14:26:05 +0200

    refactor: gtfs and osm sources

--- tasks.sh
+++ tasks.sh
@@ -1,6 +1,8 @@
 wget https://transport.data.gouv.fr/resources/79642/download -O rla.gtfs.zip
+wget https://eu.ftp.opendatasoft.com/stif/GTFS/IDFM-gtfs.zip -O idfm.gtfs.zip
 
-wget https://cdn1.arno.cl/2024%2F05%2Fnice.osm.pbf
+wget http://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf
+wget http://download.geofabrik.de/europe/france/provence-alpes-cote-d-azur-latest.osm.pbf
 
 java -jar otp-2.5.0-shaded.jar --build --save .
 
