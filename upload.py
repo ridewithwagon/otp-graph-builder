0158b30988360c214601cad1affb77f5c0160d70
Arno Cellarier <git@arno.cl>
2024-05-06 14:22:43 +0200

    feat: upload graph to a bucket

--- upload.py
+++ upload.py
@@ -1,5 +1,16 @@
+import boto3
 import os
+import io
 
-# print all files in the current directory
-for file in os.listdir('.'):
-    print(file)
+s3 = boto3.client(
+    service_name="s3",
+    endpoint_url='https://689d872a37564a5d504b7236458b95ac.r2.cloudflarestorage.com',
+    aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+    region_name="wnam",  # Must be one of: wnam, enam, weur, eeur, apac, auto
+)
+
+# Upload/Update single file
+s3.upload_fileobj(io.BytesIO(
+    open("graph.obj", "rb").read()
+), "wagon-otp-graph", "graph.obj")
