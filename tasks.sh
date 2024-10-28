ce7e4910144a740a7ba3783ddcf78e951be7a133
Arno Cellarier <git@arno.cl>
2024-10-28 10:35:59 +0100

    fix: ram limit

--- tasks.sh
+++ tasks.sh
@@ -1,14 +1,14 @@
 # On ne génère les rues que le premier du mois
 if [ $(date +%d) -eq 1 ]; then
     python3 prepare_osm.py
-    java -Xmx4G -jar otp-2.6.0-shaded.jar --buildStreet .
+    java -Xmx20G -jar otp-2.6.0-shaded.jar --buildStreet .
 else
     wget https://r2-wnam-wagon-prodassets-otp-graph.arno.cl/streetGraph.obj
 fi
 
 python3 prepare_gtfs.py
 
-java -Xmx4G -jar otp-2.6.0-shaded.jar --loadStreet --save .
+java -Xmx20G -jar otp-2.6.0-shaded.jar --loadStreet --save .
 
 mkdir external
 cp ./graph.obj ./external/graph.obj
