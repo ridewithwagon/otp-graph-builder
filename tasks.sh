2137faa7dd56694cd9a80759ff6ee43fec506854
Arno Cellarier <git@arno.cl>
2025-04-29 22:00:46 +0200

    fix: zip and missing data

--- tasks.sh
+++ tasks.sh
@@ -7,20 +7,22 @@ python3 prepare_gtfs.py
 wget https://r2-wnam-wagon-prodassets-otp-graph.arno.cl/cyprus-latest.osm.bz2
 bunzip2 cyprus-latest.osm.bz2
 pfaedle -x cyprus-latest.osm cy-kas
-zip -r cy-kas.zip gtfs-out/
+cp gtfs-out/trips.txt cy-kas/
+cp gtfs-out/stop_times.txt cy-kas/
+cp gtfs-out/shapes.txt cy-kas/
+cd cy-kas && zip -r ../cy-kas.zip . && cd ..
 s5cmd --endpoint-url="${AWS_ENDPOINT_URL}" \
   cp ./cy-kas.zip "s3://${AWS_S3_BUCKET}/cy-kas.zip"
-rm -rf cy-kas/
-mv gtfs-out cy-kas
 
 wget https://r2-wnam-wagon-prodassets-otp-graph.arno.cl/ile-de-france-latest.osm.bz2
 bunzip2 ile-de-france-latest.osm.bz2
-pfaedle -x ile-de-france-latest.osm fr-idf
-zip -r fr-idf.zip gtfs-out/
+pfaedle -m bus -x ile-de-france-latest.osm fr-idf
+cp gtfs-out/trips.txt fr-idf/
+cp gtfs-out/stop_times.txt fr-idf/
+cp gtfs-out/shapes.txt fr-idf/
+cd fr-idf && zip -r ../fr-idf.zip . && cd ..
 s5cmd --endpoint-url="${AWS_ENDPOINT_URL}" \
   cp ./fr-idf.zip "s3://${AWS_S3_BUCKET}/fr-idf.zip"
-rm -rf fr-idf/
-mv gtfs-out fr-idf
 
 java $JAVA_HEAP_OPTS -jar otp.jar --loadStreet --save .
 
